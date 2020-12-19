aed10a67d5185a09b9f008b90e438d0db2be3dd0
waldekmastykarz <waldek@mastykarz.nl>
2020-12-19 21:08:31 +0100

    Coverage 100%

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,8 +2,13 @@ import * as assert from 'assert';
 import * as foo from './index';
 
 describe('foo', () => {
-  it('runs', () => {
+  it('foo false', () => {
     foo.foo(false);
     assert(true);
-  })
+  });
+
+  it('foo true', () => {
+    foo.foo(true);
+    assert(true);
+  });
 });
